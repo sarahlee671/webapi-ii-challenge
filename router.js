a5f7a5828620b6d9d9fb2c43523768ef962712f6
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-05-28 12:12:00 -0700

    added routes for GET posts and GET posts/:id

--- router.js
+++ router.js
@@ -2,7 +2,31 @@ const db = require('./data/db.js');
 
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+    db
+        .find()
+        .then(posts => {
+            res.status(200).json({ posts });
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved."})
+        })
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        console.log(post)
+        if (post.length) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The post information could not be retrieved."});
+    }
+});
 
 
 
